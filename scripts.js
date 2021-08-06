8431a9e447602fa2ea248ab53b090533eb3d466f
Linxi Xu <xulinxi@sas.upenn.edu>
2021-08-05 20:15:39 -0400

    "2nd commit"

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 window.addEventListener("load", function () {
+    const flightStatus = document.getElementById('flightStatus');
   const takeOffBtn = document.getElementById("takeoff");
+  const shuttleBackground = document.getElementById('shuttleBackground')
+  const shuttleHeight = document.getElementById('spaceShuttleHeight')
+
   takeOffBtn.addEventListener("click", function () {
     const result = window.confirm(
       "Are you sure the shuttle is ready for takeoff?"
     );
     if (result) {
       flightStatus.innerHTML = "Shuttle in flight!";
+      shuttleBackground.style.backgroundColor = 'blue';
+      shuttleHeight.innerHTML = "10,000"
     }
   });
+  landButton.addEventListener('click', function () {
+      window.alert("The shuttle is landing. Landing gear engaged.")
+      flightStatus.innerText = "The Shuttle has landed."
+      shuttleBackground.style.backgroundColor = '#34ff70';
+      shuttleHeight.innerHTML = "0"
+  })
 });
